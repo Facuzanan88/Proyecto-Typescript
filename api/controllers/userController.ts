37e1b41055a5564dfd5dd5da88ef7d0f00c254df
Facuzanan88 <facundozanandrea@gmail.com>
2023-07-12 19:57:52 -0300

    userController

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
 import * as userService from "../services/userService";
 
-export function getAllUsers(req: Request, res: Response): void {
-  const users = userService.getAllUsers();
-  res.json(users);
+export async function getAllUsers(req: Request, res: Response): Promise<void> {
+  try {
+    const users = await userService.getUsers();
+    // console.log(users);
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export async function postUser(req: Request, res: Response) {
+  const user = req.body;
+  // console.log(user);
+  try {
+    let result = await userService.createUser(user);
+    // console.log(result);
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("We could not create the user");
+  }
 }
