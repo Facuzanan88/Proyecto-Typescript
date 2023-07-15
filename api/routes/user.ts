c00d6cfca0b0f3322553385db69fd45fe584c14f
Facuzanan88 <facundozanandrea@gmail.com>
2023-07-15 17:25:05 -0300

    refactor user

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getUsers, postUser } from "../controllers/userController";
+import { getCuts, putCut } from "../controllers/cutsController";
 
 const router = express.Router();
 
--- api/services/userService.ts
+++ api/services/userService.ts
@@ -2,10 +2,10 @@ import db from "../models";
 import { v4 as uuidv4 } from "uuid";
 import UserAtributtes from "../interfaces/user";
 
-export async function getUsers(): Promise<UserAtributtes[]> {
+export const getUsers = async (): Promise<UserAtributtes[]> => {
   let result = await db.User.findAll();
   return result;
-}
+};
 
 export const createUser = async (userData: any): Promise<UserAtributtes> => {
   let result = await db.User.create({
