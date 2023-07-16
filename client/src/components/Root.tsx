3be427bfc5acffb569ec9017b893d3f6f6ff7c17
Facuzanan88 <facundozanandrea@gmail.com>
2023-07-16 03:14:06 -0300

    manejando estados locales

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -1,21 +1,32 @@
-import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { getUsers } from "../services/usersService";
-
-const handleClick = async () => {
-  let result = await getUsers();
-  return result;
-};
+import UserAtributtes from "../interfaces/user";
 
 const Root: React.FC<{}> = () => {
+  const [user, setUser] = useState<UserAtributtes | null>();
+
+  const handleClick = async () => {
+    let result = await getUsers();
+    setUser(result);
+    return result;
+  };
+
+  const handleGet = async () => {
+    console.log(user);
+  };
+
   return (
     <>
       <div>
         <h1>FACUNDO</h1>
         <button onClick={handleClick}>Usuarios</button>
+        <button onClick={handleGet}>Mostrar</button>
       </div>
     </>
   );
 };
 
 export default Root;
+function setUser(result: any) {
+  throw new Error("Function not implemented.");
+}
--- client/src/services/usersService.tsx
+++ client/src/services/usersService.tsx
@@ -5,7 +5,7 @@ const endpoint = "users";
 export const getUsers = async () => {
   try {
     const result = await instance.get(endpoint);
-    console.log(result.data);
+    console.log(result);
     return result.data;
   } catch (err) {
     console.log(err);
