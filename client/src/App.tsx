2f559d459ecdba51855428e8780efbf8179df949
Facuzanan88 <facundozanandrea@gmail.com>
2023-07-17 18:17:08 -0300

    navBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import Root from "./components/Root";
+import Error from "./components/Error";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" Component={Root} />
-    </Routes>
+    <ChakraProvider theme={theme}>
+      <NavBar />
+      <Routes>
+        <Route path="/" Component={Root} />
+        <Route path="*" Component={Error} />
+      </Routes>
+    </ChakraProvider>
   );
 }
 
---
+++ client/src/components/Error.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error: React.FC<{}> = () => {
+  return (
+    <>
+      <div>
+        <h1>ERROR DE URL</h1>
+      </div>
+    </>
+  );
+};
+
+export default Error;
