ff66b3d394343578e4e9b999f418b4b70a092a27
Facuzanan88 <facundozanandrea@gmail.com>
2024-03-13 19:21:53 -0300

    renderizado de cards aplicando el store

--- client/src/components/Cards/Cards.tsx
+++ client/src/components/Cards/Cards.tsx
@@ -25,66 +25,82 @@ export default function ProductSimple() {
 
   return (
     <Center py={12}>
-      <Box
-        role={"group"}
-        p={6}
-        maxW={"330px"}
-        w={"full"}
-        bg={useColorModeValue("white", "gray.800")}
-        boxShadow={"2xl"}
-        rounded={"lg"}
-        pos={"relative"}
-        zIndex={1}
-      >
-        <Box
-          rounded={"lg"}
-          mt={-12}
-          pos={"relative"}
-          height={"230px"}
-          _after={{
-            transition: "all .3s ease",
-            content: '""',
-            w: "full",
-            h: "full",
-            pos: "absolute",
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${IMAGE})`,
-            filter: "blur(15px)",
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: "blur(20px)",
-            },
-          }}
-        >
-          <Image
-            rounded={"lg"}
-            height={230}
-            width={282}
-            objectFit={"cover"}
-            src={IMAGE}
-            alt="#"
-          />
-        </Box>
-        <Stack pt={10} align={"center"}>
-          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
-            {cow && cow.length !== 0 ? cow[0].name : "no existe ningun corte"}
-          </Text>
-          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-            Nice Chair, pink
-          </Heading>
-          <Stack direction={"row"} align={"center"}>
-            <Text fontWeight={800} fontSize={"xl"}>
-              $57
-            </Text>
-            <Text textDecoration={"line-through"} color={"gray.600"}>
-              $199
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
+      {cow && cow.length > 0 ? (
+        cow.map((cow, index) => {
+          // Agrega item y index como parámetros
+          return (
+            // Añade un return aquí
+            <Box
+              key={index} // Agrega una clave única para cada elemento en el array
+              role={"group"}
+              p={6}
+              maxW={"330px"}
+              w={"full"}
+              // eslint-disable-next-line react-hooks/rules-of-hooks
+              bg={useColorModeValue("white", "gray.800")}
+              boxShadow={"2xl"}
+              rounded={"lg"}
+              pos={"relative"}
+              zIndex={1}
+            >
+              <Box
+                rounded={"lg"}
+                mt={-12}
+                pos={"relative"}
+                height={"230px"}
+                _after={{
+                  transition: "all .3s ease",
+                  content: '""',
+                  w: "full",
+                  h: "full",
+                  pos: "absolute",
+                  top: 5,
+                  left: 0,
+                  backgroundImage: `url(${cow.photo})`, // Usa item.IMAGE en lugar de IMAGE
+                  filter: "blur(15px)",
+                  zIndex: -1,
+                }}
+                _groupHover={{
+                  _after: {
+                    filter: "blur(20px)",
+                  },
+                }}
+              >
+                <Image
+                  rounded={"lg"}
+                  height={230}
+                  width={282}
+                  objectFit={"cover"}
+                  src={cow.photo} // Usa item.IMAGE en lugar de IMAGE
+                  alt="#"
+                />
+              </Box>
+              <Stack pt={10} align={"center"}>
+                <Text
+                  color={"gray.500"}
+                  fontSize={"sm"}
+                  textTransform={"uppercase"}
+                >
+                  {cow.name} {/* Usa item.name */}
+                </Text>
+                <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+                  Nice Chair, pink
+                </Heading>
+                <Stack direction={"row"} align={"center"}>
+                  <Text fontWeight={800} fontSize={"xl"}>
+                    ${cow.price} {/* Usa item.price */}
+                  </Text>
+                  <Text textDecoration={"line-through"} color={"gray.600"}>
+                    $199
+                  </Text>
+                </Stack>
+              </Stack>
+            </Box>
+          );
+        })
+      ) : (
+        <Text>No hay corte</Text>
+      )}
     </Center>
   );
 }
