01185b87852c33a6f192bcfe00151b7898a7ccff
Facuzanan88 <facundozanandrea@gmail.com>
2024-03-17 20:43:15 -0300

    Diferentes secciones

--- client/src/components/QuienesSomos/QuienesSomos.tsx
+++ client/src/components/QuienesSomos/QuienesSomos.tsx
@@ -47,9 +47,24 @@ export default function QuienesSomos() {
             src="https://www.on24.com.ar/wp-content/uploads/2021/03/Ruralista.gif"
             fit={"cover"}
             align={"center"}
-            w={"60%"}
+            w={"50%"}
             h={{ base: "100%", sm: "300px", lg: "450px" }}
           />
+        </Flex>
+        <Flex justifyContent={"center"} alignItems={"center"} pb={10}>
+          <Text maxW={"50%"} textAlign="center">
+            Pampa y Novillo es una empresa familiar con más de 12 años de
+            trayectoria brindando la mejor calidad y variedad en carnes. El
+            compromiso, dedicación, evolución y adaptación al nuevo mercado nos
+            permitió crecer exponencialmente dentro del mundo cárnico,
+            instalando una planta elaboradora modelo y llegando a tener 11
+            puntos de venta de forma física y una tienda on-line, pudiendo
+            responder de manera inmediata a las necesidades de nuestros
+            clientes. Con la distribución mayorista cubrimos no solo la ciudad
+            de Mar del Plata sino también gran parte de la Costa Atlántica.
+            Seguir creciendo y que nuestros productos sean un delicioso banquete
+            en cada mesa, es nuestro mayor anhelo.
+          </Text>
         </Flex>
       </Container>
     </Box>
