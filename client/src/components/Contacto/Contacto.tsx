b82f1e24d6f4620c3fe3e1663869a99ae73db87f
Facuzanan88 <facundozanandrea@gmail.com>
2024-03-18 20:09:04 -0300

    Trabajando en el componente Contacto

--- client/src/components/Contacto/Contacto.tsx
+++ client/src/components/Contacto/Contacto.tsx
@@ -16,107 +16,96 @@ import {
   Grid,
   Input,
   VStack,
+  ChakraProvider,
+  theme,
 } from "@chakra-ui/react";
 
 import { Link } from "react-router-dom";
 import logo from "../../Assest/logo.jpg";
 
-export default function QuienesSomos() {
+function ContactSection() {
   return (
-    <Box bg={useColorModeValue("gray.100", "gray.700")}>
-      <Stack
-        spacing={0}
-        align={"center"}
-        maxH={"full"}
-        bg={useColorModeValue("white", "black")}
-        color={useColorModeValue("black", "white")}
-      >
-        <Text
-          bg={useColorModeValue("white", "black")}
-          color={useColorModeValue("black", "white")}
-          maxH={"full"}
-          fontSize={"xs"}
-        >
-          LOS PEDIDOS SE ENTREGAN AL OTRO DÍA DE REALIZADA LA COMPRA. NO HAY
-          ENTREGAS LOS DOMINGOS.
-        </Text>
-      </Stack>
-      <Container maxW={"full"}>
-        <Stack spacing={0} align={"center"} mt={4}>
-          <Heading>QUIENES SOMOS?</Heading>
+    <Stack
+      spacing={4}
+      maxH={"full"}
+      color={useColorModeValue("black", "white")}
+    >
+      <Flex justify="space-between" mx={20}>
+        <Stack>
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            Contactos:
+          </Text>
+          <Text>Correo electrónico: contacto@pampaynovillo.com</Text>
+          <Text>Teléfono: +54 11 1234 5678</Text>
+          {/* Agrega aquí más información de contacto si es necesario */}
         </Stack>
-
-        <Flex justifyContent={"center"} alignItems={"center"}>
-          <Image
-            my={6}
-            rounded={"md"}
-            alt={"product image"}
-            src="https://www.on24.com.ar/wp-content/uploads/2021/03/Ruralista.gif"
-            fit={"cover"}
-            align={"center"}
-            w={"100%"}
-            h={{ base: "100%", sm: "300px", lg: "250px" }}
-          />
-        </Flex>
-        <Flex justifyContent={"space-between"} align={"center"} p={4}>
-          {/* Contactos y redes sociales */}
-          <Stack
-            spacing={4}
-            align={"center"}
-            maxH={"full"}
-            color={useColorModeValue("black", "white")}
-          >
-            <Text fontSize={"lg"} fontWeight={"bold"}>
-              Contactos:
-            </Text>
-            <Text>Correo electrónico: contacto@pampaynovillo.com</Text>
-            <Text>Teléfono: +54 11 1234 5678</Text>
-            {/* Agrega aquí más información de contacto si es necesario */}
-            <Text fontSize={"lg"} fontWeight={"bold"}>
-              Redes Sociales:
-            </Text>
-            <HStack spacing={4}>
-              <Link to={`/facebook`}>Facebook</Link>
-              <Link to={"#"}>Instagram</Link>
-              {/* Agrega aquí más enlaces a redes sociales si es necesario */}
-            </HStack>
-          </Stack>
-
-          {/* Formulario de sugerencias */}
-          <Stack
-            spacing={4}
-            align={"center"}
-            maxH={"full"}
-            color={useColorModeValue("black", "white")}
-          >
-            <Text fontSize={"lg"} fontWeight={"bold"}>
-              Envía tus sugerencias o consultas:
-            </Text>
-            <form>
-              <VStack spacing={4} align="stretch" alignItems="center">
-                <FormControl id="name" w="50%">
-                  <FormLabel>Nombre</FormLabel>
-                  <Input type="text" name="name" />
-                </FormControl>
-
-                <FormControl id="lastname" w="50%">
-                  <FormLabel>Apellido</FormLabel>
-                  <Input type="text" name="lastname" />
-                </FormControl>
-
-                <FormControl id="age" w="50%">
-                  <FormLabel>Edad:</FormLabel>
-                  <Input type="number" name="age" />
-                </FormControl>
-
-                <Grid pb="4">
-                  <Button type="submit">Guardar</Button>
-                </Grid>
-              </VStack>
-            </form>
-          </Stack>
-        </Flex>
-      </Container>
-    </Box>
+        <Stack>
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            Redes Sociales:
+          </Text>
+          <HStack spacing={4}>
+            <Link to={`/facebook`}>Facebook</Link>
+            <Link to={"#"}>Instagram</Link>
+            {/* Agrega aquí más enlaces a redes sociales si es necesario */}
+          </HStack>
+        </Stack>
+      </Flex>
+    </Stack>
   );
 }
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Flex justifyContent={"space-between"} align={"center"} p={4}>
+        {/* Contactos y redes sociales */}
+        <Stack
+          spacing={4}
+          align={"center"}
+          maxH={"full"}
+          color={useColorModeValue("black", "white")}
+          width="40%"
+        >
+          <ContactSection />
+        </Stack>
+
+        {/* Formulario de sugerencias */}
+        <Stack
+          spacing={4}
+          align={"center"}
+          maxH={"full"}
+          color={useColorModeValue("black", "white")}
+          width="60%"
+        >
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            Envía tus sugerencias o consultas:
+          </Text>
+          <form>
+            <VStack spacing={4} align="stretch" alignItems="center">
+              <FormControl id="name">
+                <FormLabel>Nombre</FormLabel>
+                <Input type="text" name="name" />
+              </FormControl>
+
+              <FormControl id="lastname">
+                <FormLabel>Apellido</FormLabel>
+                <Input type="text" name="lastname" />
+              </FormControl>
+
+              <FormControl id="age">
+                <FormLabel>Edad:</FormLabel>
+                <Input type="number" name="age" />
+              </FormControl>
+
+              <Grid pb="4">
+                <Button type="submit">Guardar</Button>
+              </Grid>
+            </VStack>
+          </form>
+        </Stack>
+      </Flex>
+    </ChakraProvider>
+  );
+}
+
+export default App;
