98b2c724ea66a7b93669b988afdf5d2ebea1eb37
Facuzanan88 <facundozanandrea@gmail.com>
2024-04-25 20:07:51 -0300

    models

--- api/models/User.ts
+++ api/models/User.ts
@@ -31,7 +31,7 @@ module.exports = (sequelize: any, DataTypes: any) => {
       },
       lastname: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       photo: {
         type: DataTypes.STRING,
