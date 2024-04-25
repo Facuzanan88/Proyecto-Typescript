053bb7179132a618cd577ad69f25818fcdd26ac9
Facuzanan88 <facundozanandrea@gmail.com>
2024-04-25 18:11:38 -0300

    funcion userByMail

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { getUsers, postUser, putUser } from "../controllers/userController";
+import {
+  getUsers,
+  postUser,
+  putUser,
+  userByMail,
+} from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.get("/users/:id", getUsers);
+router.get("/userid", userByMail);
 router.post("/users", postUser);
 router.put("/users", putUser);
 
