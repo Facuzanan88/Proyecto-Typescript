5f2949c05735d7b4fe2e2b4b20272c115e6fedef
Facuzanan88 <facundozanandrea@gmail.com>
2024-04-26 17:42:01 -0300

    registro y sus funciones

--- client/src/components/Registro/Registro.tsx
+++ client/src/components/Registro/Registro.tsx
@@ -21,20 +21,25 @@ const Registro: React.FC<{}> = () => {
   const [captchaValido, setCaptchaValido] = useState(true);
   const [usuarioValido, setUsuarioValido] = useState(false);
 
+  const [usuario, setUsuario] = useState<UserAtributtes>({
+    id: undefined,
+    name: undefined,
+    lastname: undefined,
+    photo: undefined,
+    age: undefined,
+    email: undefined,
+    cel: undefined,
+    street: undefined,
+    number: undefined,
+    apartment: undefined,
+    comment: undefined,
+  });
+
+  const [isChecked, setIsChecked] = useState(usuario?.apartment);
+
   const [apartment, setApartment] = useState(false);
 
-  const [usuario, setUsuario] = useState<UserAtributtes>({
-    name: "",
-    lastname: "",
-    photo: "",
-    age: 0,
-    email: "",
-    cel: 0,
-    street: "",
-    number: 0,
-    apartment: false,
-    comment: "",
-  });
+  const [id, setId] = useState(usuario.id);
 
   const navigate = useNavigate();
 
@@ -49,25 +54,30 @@ const Registro: React.FC<{}> = () => {
         if (oldUser && oldUser.email) {
           setUsuario({
             ...usuario,
+            id: oldUser.id,
             name: oldUser.name,
             lastname: oldUser.lastname,
             photo: oldUser.photo,
+            age: oldUser.age,
             email: oldUser.email,
+            cel: oldUser.cel,
+            street: oldUser.street,
+            number: oldUser.number,
+            apartment: oldUser.apartment,
+            comment: oldUser.comment,
           });
-          console.log(oldUser, "Olduser");
-        } else {
-          const newUser = await UserStore.createUser(user);
-          console.log(newUser, "newUser");
+          setIsChecked(oldUser.apartment);
         }
+        console.log("piki");
       }
     };
 
-    fetchData(); // Llama a la función fetchData para iniciar la operación asincrónica
-  }, [user]); // Agrega user como dependencia para que el useEffect se ejecute cuando user cambie
+    fetchData();
+  }, [user, UserStore.modifyUser]);
 
   const onChange = () => {
     if (captcha.current?.getValue()) {
-      console.log("el usuario no es un robot", captcha.current.getValue());
+      console.log("el usuario no es un robot");
       setCaptchaValido(true);
     }
   };
@@ -77,17 +87,13 @@ const Registro: React.FC<{}> = () => {
     setUsuario({ ...usuario, [name]: value });
   };
 
-  const handleInputCheck = (event: { target: { name: any; checked: any } }) => {
-    const { name, checked } = event.target;
-    if (name === "apartment") {
-      setApartment(checked);
-    }
+  const handleInputCheck = (event: any) => {
+    setIsChecked(event.target.checked);
+    // Aquí puedes realizar cualquier otra acción que necesites con el estado actualizado
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const idUser = UserStore.user.id;
 
     if (typeof usuario.age === "string") {
       usuario.age = parseFloat(usuario.age);
@@ -98,12 +104,10 @@ const Registro: React.FC<{}> = () => {
     if (typeof usuario.number === "string") {
       usuario.number = parseFloat(usuario.number);
     }
-
-    usuario.apartment = apartment;
+    usuario.apartment = isChecked; // NO FUNCIONA EL isChecked
 
     if (captcha.current?.getValue()) {
       console.log("el usuario no es un robot");
-      console.log(idUser);
       setUsuarioValido(true);
       setCaptchaValido(true);
       Swal.fire({
@@ -114,15 +118,19 @@ const Registro: React.FC<{}> = () => {
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!",
-      }).then((result) => {
-        if (result.isConfirmed && idUser) {
-          UserStore.modifyUser(idUser, usuario);
-          Swal.fire({
-            title: "Perfil Guardado",
-            text: "Sus datos han sido guardados correctamente",
-            icon: "success",
-          });
+      }).then(async (result) => {
+        if (result.isConfirmed && user?.email) {
+          const newUser = await UserStore.userByMail(user.email);
+          if (newUser && newUser.id) {
+            await UserStore.modifyUser(usuario);
+            console.log("Modificar");
+          }
         }
+        Swal.fire({
+          title: "Perfil Guardado",
+          text: "Sus datos han sido guardados correctamente",
+          icon: "success",
+        });
       });
     } else {
       console.log("primero debes completar el CAPTCHA");
@@ -220,7 +228,7 @@ const Registro: React.FC<{}> = () => {
           <FormLabel>Departamento</FormLabel>
           <Checkbox
             name="apartment"
-            isChecked={apartment}
+            isChecked={isChecked}
             onChange={handleInputCheck}
           />
         </FormControl>
